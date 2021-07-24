f71177bd2d6f348594f3942cb843fde40ce65972
Dipesh Paul <dipeshpal17@gmail.com>
2021-07-24 14:17:29 +0530

    Update strea.py

--- strea.py
+++ strea.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pyngrok import ngrok
+import requests
 
 url = "None"
 if not hasattr(st, 'already_started_server'):
@@ -24,14 +25,15 @@ if not hasattr(st, 'already_started_server'):
     def serve_foo():
         return 'This page is served via Flask!'
 
-    url = ngrok.connect(8888).public_url
-    print(' * Tunnel URL:', url)
 
+    endpoint = ngrok.connect(8888).public_url
+    print(' * Tunnel URL:', endpoint)
+    status = requests.get(
+        f"https://jarvis-ai-api.herokuapp.com/update_api_endpoint/?username=dipeshpal&token=5d57286c59a3c6d8c30e1d6675c0a6&endpoint={endpoint}")
+    print(status)
     app.run(port=8888)
 
-
 # We'll never reach this part of the code the first time this file executes!
-
 # Your normal Streamlit app goes here:
 x = st.slider('Pick a number')
 st.write('You picked:', x)
